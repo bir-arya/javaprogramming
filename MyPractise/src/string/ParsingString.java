b8d5421da5aa0c7847705224abbd60a02b746152
bir-arya <birjooarya@gmail.com>
2024-10-23 16:34:26 +0530

    new changes

--- MyPractise/src/string/ParsingString.java
+++ MyPractise/src/string/ParsingString.java
@@ -11,6 +11,17 @@ public class ParsingString {
 		System.out.println(a);	
 		
 		double d = Double.parseDouble(s);
+		
+		long l = Long.parseLong(s);
+		
+		System.out.println(d);
+		System.out.println(l);
+		
+		
+		int a1 = 10;
+		
+		String s1 = String.valueOf(a);
+		System.out.println(a1);
 	}
 
 }
